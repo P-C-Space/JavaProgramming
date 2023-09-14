252543ab9abbf4e70df9e84ac6e2d674918a5a45
jeongwoo <sun62073812@gmail.com>
2023-09-14 15:00:16 +0900

    3장 문제풀이 + gitignore

---
+++ Ch3/ProgrammingExam/P3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        double a = 0.0;
+        double b = 0.0;
+        double result = 0;
+        String op = " ";
+
+        while(true){
+            a = s.nextDouble();
+            if(a == 0){
+                break;
+            }
+            op = s.next();
+            b = s.nextDouble();
+
+            switch (op){
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a + b;
+                    break;
+                case "*":
+                    result = a + b;
+                    break;
+                case "/":
+                    result = a + b;
+                    break;
+            }
+            System.out.println(a + " " + op + " " + b + " = " + result);
+        }
+    }
+}
