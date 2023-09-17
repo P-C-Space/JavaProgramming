eb7a9000ce452160fdae963297bcd22df9de4de7
jeongwoolee <sun62073812@gmail.com>
2023-09-18 01:20:57 +0900

    Ch5 exercise1

---
+++ Ch5/src/exercise/Exercise1.java
@@ -0,0 +1,18 @@
+package Ch5.src.exercise;
+
+public class Exercise1 {
+
+}
+
+class pairOfDice{
+    private int die1;
+    private int die2;
+
+    public int getDie1() {
+        return die1;
+    }
+
+    public int getDie2() {
+        return die2;
+    }
+}
