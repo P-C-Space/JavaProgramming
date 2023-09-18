8b0a2fe7b4bf01d802ea9484b2b6a49f0228e2fc
jeongwoo <sun62073812@gmail.com>
2023-09-18 13:30:54 +0900

    Code : Ch5 Exercise

--- Ch5/src/exercise/Exercise1.java
+++ Ch5/src/exercise/Exercise1.java
@@ -1,18 +1,10 @@
-package Ch5.src.exercise;
+package exercise;
 
 public class Exercise1 {
 
-}
-
-class pairOfDice{
-    private int die1;
-    private int die2;
-
-    public int getDie1() {
-        return die1;
-    }
-
-    public int getDie2() {
-        return die2;
+    public static void main(String[] args) {
+        PairOfDice pairOfDice = new PairOfDice();
+        pairOfDice.findValue();
     }
 }
+
