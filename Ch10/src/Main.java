3c8eb7d9547ed0edd1d56f921c64c35a2f85ee31
jeongwoolee <sun62073812@gmail.com>
2023-09-28 14:57:21 +0900

    Update

--- Ch10/src/Main.java
+++ Ch10/src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    class PhoneDirectory{
+        private TreeMap<String,String> data;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
